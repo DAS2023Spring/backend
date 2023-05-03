0ddf55b5d4d5fc5fb598e74b8168327727b7d6fa
Mahdi Jafari <mahdi.jfri.79@gmail.com>
2023-05-03 14:29:07 +0330

    Introduce login, register, change password for users

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,12 @@
-urlpatterns = [
+import rest_framework.authtoken.views
+from django.urls import path
 
+from . import views
+
+app_name = 'user'
+
+urlpatterns = [
+    path('register/', views.RegisterAPIView.as_view(), name='register'),
+    path('login/', rest_framework.authtoken.views.obtain_auth_token, name='login'),
+    path('reset-password/<str:username>/', views.ResetPasswordAPIView.as_view(), name='reset-password'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,94 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.db import transaction
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import get_error_detail
+from rest_framework.generics import CreateAPIView, GenericAPIView, get_object_or_404
+from django.core.exceptions import ValidationError as DjangoValidationError
+from rest_framework.response import Response
 
-# Create your views here.
+from user.models import SecurityQuestion
+
+
+class SecurityQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SecurityQuestion
+        fields = [
+            'question',
+            'answer',
+        ]
+        extra_kwargs = {
+            'answer': {'write_only': True},
+        }
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    security_question = SecurityQuestionSerializer(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password',
+            'security_question'
+        ]
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def validate_password(self, password):
+        try:
+            validate_password(password)
+        except DjangoValidationError as ve:
+            raise ValidationError(get_error_detail(ve))
+
+    @transaction.atomic
+    def create(self, validated_data):
+        security_question_data = validated_data.pop('security_question')
+        user = User.objects.create_user(username=validated_data["username"], password=validated_data["password"])
+        security_question_data['user'] = user
+        self.fields['security_question'].create(security_question_data)
+        return user
+
+
+class ResetPasswordSerializer(serializers.Serializer):
+    password = serializers.CharField()
+    answer = serializers.CharField()
+
+    def validate_password(self, password):
+        try:
+            validate_password(password)
+        except DjangoValidationError as ve:
+            raise ValidationError(get_error_detail(ve))
+
+    def validate_answer(self, answer):
+        return self.context['security_question'].answer.strip() == answer.strip()
+
+    class Meta:
+        fields = ['password', 'answer']
+
+
+class RegisterAPIView(CreateAPIView):
+    serializer_class = RegisterSerializer
+
+
+class ResetPasswordAPIView(GenericAPIView):
+    queryset = User.objects.all()
+    lookup_field = 'username'
+
+    def get(self, request, *args, **kwargs):
+        user = self.get_object()
+        security_question = get_object_or_404(SecurityQuestion, user=user)
+        return Response(SecurityQuestionSerializer(instance=security_question).data)
+
+    def post(self, request, *args, **kwargs):
+        user: User = self.get_object()
+        security_question = get_object_or_404(SecurityQuestion, user=user)
+        serializer = ResetPasswordSerializer(
+            request.data,
+            context={**self.get_serializer_context(), 'security_question': security_question}
+        )
+        serializer.is_valid(raise_exception=True)
+        user.set_password(serializer.data['answer'])
+        return Response()
